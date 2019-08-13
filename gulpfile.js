d7054e037dfc0f218084cf21926561ee54cfa876
Yuki Tomioka <yuki.tomioka.1016@gmail.com>
2019-08-13 11:39:10 +0900

    gulpの設定

---
+++ gulpfile.js
@@ -0,0 +1,73 @@
+/**
+ * ------------------------------------------------------------
+ * 読み込み
+ * ------------------------------------------------------------
+ */
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const inlineCss = require('gulp-inline-css');
+const browserSync = require('browser-sync').create();
+
+const src = './src';
+const dist = './dist';
+
+/**
+ * ------------------------------------------------------------
+ * 設定
+ * ------------------------------------------------------------
+ */
+const sassOption = {
+  outputStyle: 'expanded'
+}
+
+const inlineCssOption = {
+  applyStyleTags: false,
+  removeStyleTags: false
+};
+
+const browserSyncOption = {
+  server: './dist'
+};
+
+
+/**
+ * ------------------------------------------------------------
+ * タスク
+ * ------------------------------------------------------------
+ */
+gulp.task('sass', () => {
+  return gulp
+    .src(src + '/sass/**/*.scss')
+    .pipe(sass(sassOption))
+    .pipe(gulp.dest(src + '/css/'));
+});
+
+gulp.task('inlineCss', () => {
+  return gulp
+    .src(src + '/**/*.html')
+    .pipe(inlineCss(inlineCssOption))
+    .pipe(gulp.dest(dist))
+});
+
+gulp.task('serve', (done) => {
+  browserSync.init(browserSyncOption);
+  done();
+});
+
+/**
+ * ------------------------------------------------------------
+ * watchタスク
+ * ------------------------------------------------------------
+ */
+gulp.task('watch', (done) => {
+  const browserReload = (done) => {
+    browserSync.reload();
+    done();
+  };
+  gulp.watch(src + '/**/*.scss', gulp.series('sass', 'inlineCss'));
+  gulp.watch(src + '/**/*.html', gulp.task('inlineCss'));
+  gulp.watch(dist + '/*', browserReload);
+});
+
+
+gulp.task('default', gulp.series('serve', 'watch'));
